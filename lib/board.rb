bb4519e12ed6757f046f2d0b36beb92568c745e8
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-09-02 18:57:33 -0400

    feat: add Board class with initial methods
    - initialize board with empty array
    - display_board to print the formatted board
    - update_board to add markers to cells

---
+++ lib/board.rb
@@ -0,0 +1,37 @@
+# Represents the game board, handles rendering, and manages the state of the board
+class Board
+  attr_reader :board
+
+  def initialize
+    @board = Array.new(9, "-")
+  end
+
+  def display_board
+    puts "\n"
+    @board.each_slice(3) { |row| puts row.join(" | ") }
+    puts "\n"
+  end
+
+  def update_board(cell, marker)
+    if valid_move?(cell)
+      @board[cell] = marker
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(cell)
+    cell.between?(0, 8) && @board[cell] == "-"
+  end
+
+  def board_full?
+    !@board.include?("-")
+  end
+end
+
+game = Board.new
+
+puts game.display_board
+game.update_board(0, "X")
+puts game.display_board
